1658e6186c35560b33452fd0b55fb6c6ebc37310
KarlH3644 <43153810+karl3644@users.noreply.github.com>
2023-08-25 16:22:52 +0100

    section 3 tutorial all components added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Auth / MSAL
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <MsalProvider instance={msalInstance}>
     <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
