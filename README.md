4d0c57154a817f1f9d6491ac01fda4e3cbefb0c1
KarlH3644 <43153810+karl3644@users.noreply.github.com>
2023-09-04 16:30:00 +0100

    notes

--- README.md
+++ README.md
@@ -9,8 +9,6 @@ Installed @azure/msal-browser @azure/msal-react
 
 Added authConfig with clientId, authority & redirectUri
 
-[current progress](https://learn.microsoft.com/en-us/azure/active-directory/develop/single-page-app-tutorial-03-sign-in-users?tabs=visual-studio-code)
-
 ```js
 const isAuthenticated = useIsAuthenticated();
 ```
--- src/authConfig.js
+++ src/authConfig.js
@@ -10,9 +10,6 @@ import { LogLevel } from "@azure/msal-browser";
  * For a full list of MSAL.js configuration parameters, visit:
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
  */
-
-console.log('process.env.REACT_APP_AZURE_CLIENT_ID', process.env.REACT_APP_AZURE_CLIENT_ID);
-console.log('process.env.REACT_APP_AZURE_TENANT_ID', process.env.REACT_APP_AZURE_TENANT_ID);
 export const msalConfig = {
     auth: {
         clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
